4c8dcbad7efed2881ba4bb337ea85a8107b64e10
Gabriel <gabriel.gsm2017@gmail.com>
2022-05-21 16:07:32 -0300

    getting user data

--- front-end/src/components/Main/index.jsx
+++ front-end/src/components/Main/index.jsx
@@ -2,20 +2,45 @@ import React, { useState } from "react";
 import * as S from "./styles";
 
 function Main() {
+  const [userData, setUserData] = useState({});
+  const [valueFields, setValueFields] = useState();
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setUserData((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setValueFields("");
+    console.log(userData);
+  };
   return (
-    <S.Wrapper>
+    <S.Wrapper onSubmit={onSubmit}>
       <S.Title>Cadastro</S.Title>
       <S.Form>
         <S.Label>Nome</S.Label>
-        <S.Input />
-        <S.Label>E-mail</S.Label>
-        <S.Input />
-        <S.Label>Nascimento</S.Label>
-        <S.Input />
-        <S.Label>Telefone</S.Label>
-        <S.Input />
+        <S.Input value={valueFields} name="userName" onChange={handleChange} />
 
-        <S.Button>Cadastrar</S.Button>
+        <S.Label>E-mail</S.Label>
+        <S.Input value={valueFields} name="userEmail" onChange={handleChange} />
+
+        <S.Label>Nascimento</S.Label>
+        <S.Input
+          value={valueFields}
+          type="date"
+          name="userNascimento"
+          onChange={handleChange}
+        />
+
+        <S.Label>Telefone</S.Label>
+        <S.Input
+          value={valueFields}
+          name="userTelefone"
+          onChange={handleChange}
+        />
+
+        <S.Button type="submit">Cadastrar</S.Button>
       </S.Form>
     </S.Wrapper>
   );
